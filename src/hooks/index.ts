1e248bb539f9f3ea202185f89716aa420f477ad5
Wyatt Bai <53634020+congweibai@users.noreply.github.com>
2023-09-26 21:39:38 +0930

    feat: update index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,5 @@ export * from "./useDocumentTitle/useDocumentTitle";
 export * from "./useDefault/useDefault";
 export * from "./useToggle/useToggle";
 export * from "./usePrevious/usePrevious";
+export * from "./useCounter/useCounter";
+export * from "./usePreferredLanguage/usePreferredLanguage";

e7d109e25fdc726bee1f50e674d2f7da318d9b59
Wyatt Bai <53634020+congweibai@users.noreply.github.com>
2023-10-03 20:57:44 +1030

    feat: useCopyToClipboard

---
+++ src/hooks/useCopyToClipboard/useCopyToClipboard.test.tsx
@@ -0,0 +1,26 @@
+import { afterEach, describe, expect, it, test, vitest } from "vitest";
+import useCopyToClipboard from "./useCopyToClipboard";
+import { act, renderHook, waitFor } from "@testing-library/react";
+
+document.execCommand = vitest.fn();
+describe("useCopyToClipboard", () => {
+  afterEach(() => {
+    vitest.clearAllMocks();
+  });
+  it("should return an array with the first item being the value that was copied and the second being a function to copy a value to the user's clipboard", () => {
+    const { result } = renderHook(() => useCopyToClipboard());
+
+    expect(result.current[0]).toBeNull();
+    expect(typeof result.current[1]).toBe("function");
+  });
+
+  test("The first item in the array that useCopyToClipboard returns should be the value that was copied to the clipboard", async () => {
+    const { result } = renderHook(() => useCopyToClipboard());
+
+    act(() => {
+      result.current[1]("testValue");
+    });
+
+    await waitFor(async () => expect(result.current[0]).toBe("testValue"));
+  });
+});
